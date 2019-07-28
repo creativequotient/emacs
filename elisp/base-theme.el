f9a189bb4e76876a6d7d1878c10f797cc5cf5460
Marcus Lee <marcus_lee@u.nus.edu>
2019-07-28 15:05:33 +0800

    more updates

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-(use-package leuven-theme
-  :ensure nil
-=======
 (use-package zenburn-theme
   :defer t
->>>>>>> cf61fa7a8cbde2c927e7cb75ccec263f27a50534
   :init
   (load-theme 'zenburn t))
 
