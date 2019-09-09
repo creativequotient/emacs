77db70fa5ef02401cd70c06610b7d7068fae0f69
Marcus Lee <marcus_lee@u.nus.edu>
2019-09-09 12:55:22 +0800

    Update

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 ;; (use-package leuven-theme
 ;;   :defer t
 ;;   :init
 ;;   (load-theme 'leuven t))
 
 (load-theme 'atom-one-dark t)
-=======
-(load-theme 'tango-dark)
->>>>>>> da51498446e26d5707bc768e222f8ffcff0148a3
 
 (provide 'base-theme)
