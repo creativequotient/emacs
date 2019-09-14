3b9ace9530d1f53daaf0a29ffdee6ca87d4e1f0a
Marcus Lee <marcus_lee@u.nus.edu>
2019-09-14 16:09:22 +0800

    Update

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,8 +1,8 @@
-;; (use-package leuven-theme
-;;   :defer t
-;;   :init
-;;   (load-theme 'leuven t))
+(use-package leuven-theme
+  :defer t
+  :init
+  (load-theme 'leuven t))
 
-(load-theme 'atom-one-dark t)
+;; (load-theme 'atom-one-dark t)
 
 (provide 'base-theme)
