d89091425bc6641512845f17eba776a0c908c77b
Marcus Lee <marcus_lee@u.nus.edu>
2020-12-29 10:51:08 +0800

    Added helpful package

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -37,6 +37,16 @@
   :config
   (hlinum-activate))
 
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 (use-package ivy
   :bind
   ("C-x s" . swiper)
